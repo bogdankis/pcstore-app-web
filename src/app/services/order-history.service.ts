2bc52b6d4876e4df288d338abb3313d6ddbaf3b8
bogdankis <chismbogdan@gmail.com>
2022-08-07 19:37:49 +0300

    repaired typo order-history endpoint

--- src/app/services/order-history.service.ts
+++ src/app/services/order-history.service.ts
@@ -8,13 +8,13 @@ import { OrderHistory } from '../common/order-history';
 })
 export class OrderHistoryService {
 
-  private orderUrl = 'https://localhost:8080/api/orders'
+  private orderUrl = 'http://localhost:8080/api/orders'
 
   constructor(private httpClient: HttpClient) { }
 
   getOrderHistory(theEmail: string): Observable<GetResponseOrderHistroy>{
     // build URL base on customer email
-    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesecending?email=${theEmail}`;
+    const orderHistoryUrl = `${this.orderUrl}/search/findByCustomerEmailOrderByDateCreatedDesc?email=${theEmail}`;
 
     return this.httpClient.get<GetResponseOrderHistroy>(orderHistoryUrl);
   }
