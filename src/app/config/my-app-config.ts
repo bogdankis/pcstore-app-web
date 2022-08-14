b37b20b05e86e31c8472e2da1cecb782770e4c41
bogdankis <chismbogdan@gmail.com>
2022-08-14 15:17:27 +0300

    configuration of URL for okta in package.json

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,30 @@ export default {
     oidc: {
         clientId: '0oaq6dsb9xhj0Wany5d6',
         issuer: 'https://dev-91311639.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid','profile', 'email']
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
